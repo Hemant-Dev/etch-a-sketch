3450360904c0966fa083f19f4f1b0ff940e8569a
Hemant-Dev <phemant092@gmail.com>
2023-12-22 21:01:22 +0530

    Added size functionality: 1.Added 3 size options along with improved aspect - ratio 2.Refactorized various functions into a function to make it reusable

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('.container');
 const gameWindow = document.querySelector('.game-container');
-const gridSize = 6;
 const colorBtn = document.querySelector('.colorBtn');
 const eraseBtn = document.querySelector('.eraseBtn');
+const sixBtn = document.querySelector('.six');
+const twelveBtn = document.querySelector('.twelve');
+const twentyFourBtn = document.querySelector('.twentyFour');
 
-
+let gridSize = 6;
 //Created obj
 createColumns(gridSize);
 function createColumns(gridSize){
@@ -29,7 +31,7 @@ function createRow(gridSize, row){
 }
 
 //Logic to change Color when changeColor Btn is clicked
-let color = '';   //Global color var that it uses
+let color = 'red';   //Global color var that it uses
 colorBtn.addEventListener('click', changeColor);
 function changeColor(e){
     color = e.target.value;
@@ -39,21 +41,81 @@ function getColor(){
 }
 
 //Logic to change color on entry in the grid div
-
-let gridList = document.querySelectorAll('.grid-row');
+function getGrid(){
+    const gridList = document.querySelectorAll('.grid-row');
+    return Array.from(gridList);
+}
+let gridList = getGrid();
 // console.log(gridList);
-Array.from(gridList);
+function coloringTheGrid(){
+    gridList = getGrid();
+    gridList.forEach((element) => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = getColor();
+        });
+    });
+}
 gridList.forEach((element) => {
     element.addEventListener('mouseover', () => {
         element.style.backgroundColor = getColor();
     });
-})
-
+});
 
 //Logic to Reset the whole grid to initial state
-eraseBtn.addEventListener('click', reset);
-function reset(){
+eraseBtn.addEventListener('click', erase);
+function erase(){
     gridList.forEach((element) => {
         element.style.backgroundColor = 'white';
     });
+}
+
+
+//Logic to change Grid Size using button provided
+sixBtn.addEventListener('click', () => {
+    resetGrid();
+    createColumns(gridSize = 6);
+    changeGridDimensions();
+    coloringTheGrid();
+});
+twelveBtn.addEventListener('click', () => {
+    resetGrid();
+    gridSize = 12;
+    createColumns(gridSize);
+    changeGridDimensions();
+    coloringTheGrid();
+});
+twentyFourBtn.addEventListener('click', () => {
+    resetGrid();
+    gridSize = 24;
+    createColumns(gridSize);
+    changeGridDimensions();
+    coloringTheGrid();
+});
+
+
+//Logic to reset the whole grid
+function resetGrid(){
+    const gameWindow = document.querySelector('.game-container');
+    Array.from(gameWindow);
+    while(gameWindow.firstChild){
+        gameWindow.firstChild.remove();
+    }
+}
+
+function changeGridDimensions(){
+    const gridList = document.querySelectorAll('.grid');
+    Array.from(gridList);
+    let gridDimensions = 80 + 'px';
+    if(gridSize === 12)
+        gridDimensions = 40 + 'px';
+    else if(gridSize === 24)
+        gridDimensions = 20 + 'px';
+    else
+        gridDimensions = 80 + 'px';
+        
+    gridList.forEach((element) => {
+        element.style.height = gridDimensions;
+        element.style.width = gridDimensions;
+    });
+
 }
